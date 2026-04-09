092656856a1ce5c3d4899c13a39b9d1bec1256fb
why7ss7why-max <why7ss7why@gmail.com>
2026-04-09 23:12:14 +0600

    Setup GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/why7ss.github.io/',  // Замени 'easygame' на название твоего репозитория
   plugins: [react()],
   server: {
     port: 3000,
